9893d33e8ef9b8d7ae290cbe84a7f2908efc5d43
vyshak15sp <spvyshak@gmail.com>
2023-05-18 08:41:19 +0530

    Changes have been made to the code to fix the bottom scroll to fix the window

--- script/validationEmail.js
+++ script/validationEmail.js
@@ -1,6 +1,8 @@
 emailjs.init('5rcemQjiw5ewrra4d');
 
 const form = document.getElementById('contact-form'); // Replace 'myForm' with your form ID
+const successMessageElement = document.getElementById('successMessage');
+
 
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent form submission
@@ -14,8 +16,58 @@ form.addEventListener('submit', function(event) {
   }
 
   // If reCAPTCHA is completed, proceed to send the form data
-  sendFormData();
+  if (validateForm()) {
+    sendFormData();
+  }
 });
+function validateName(event){
+  const name = document.getElementById('name');
+
+  if (name.value.trim() === '') {
+    name.classList.remove('valid');
+    name.classList.add('invalid');
+    isValid = false;
+  } else {
+    name.classList.remove('invalid');
+    name.classList.add('valid');
+  }
+
+}
+function validateForm() {
+  const email = document.getElementById('email');
+  const message = document.getElementById('message');
+
+  let isValid = true;
+
+  // Validate name field
+  
+  // Validate email field
+  if (email.value.trim() === '' || !isValidEmail(email.value.trim())) {
+    email.classList.remove('valid');
+    email.classList.add('invalid');
+    isValid = false;
+  } else {
+    email.classList.remove('invalid');
+    email.classList.add('valid');
+  }
+
+  // Validate message field
+  if (message.value.trim() === '') {
+    message.classList.remove('valid');
+    message.classList.add('invalid');
+    isValid = false;
+  } else {
+    message.classList.remove('invalid');
+    message.classList.add('valid');
+  }
+  return isValid;
+}
+
+function isValidEmail(email) {
+  // Use a regular expression to validate email format
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
 
 function sendFormData() {
   // Capture form data
@@ -34,18 +86,14 @@ function sendFormData() {
   emailjs.send('service_y21mze4', 'template_50bdudl', templateParams)
     .then(function(response) {
       console.log('Email sent successfully:', response);
-      alert('Your message has been sent!');
+      // Show success message in the contact us modal
+      successMessageElement.innerText = 'Your message has been sent!';
+      successMessageElement.style.display = 'block';
+      // $('#successModal').modal('show');
+      // alert("The message has been sucessfully sent.")
 
       // Reset the form
-
-      const oldForm = document.getElementById('contact-form');
-      const newForm = document.createElement('form');
-      newForm.id = 'contact-form';
-      newForm.innerHTML = oldForm.innerHTML;
-      oldForm.parentNode.replaceChild(newForm, oldForm);
-
-
-      // document.getElementById('contact-form').reset();
+        form.reset();
       
       // Clear the reCAPTCHA response
       grecaptcha.reset();
@@ -54,7 +102,9 @@ function sendFormData() {
     })
     .catch(function(error) {
       console.log('Email sending failed:', error);
-      alert('There was an error sending your message. Please try again later.');
+      // $('#errorModal').modal('show');
+      alert("oops!.")
+
       // Handle error message or any further actions
     });
 }
